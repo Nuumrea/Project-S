76c7d3cc2bb1664618a5f682d4293b1254f28111
nuumrea <kevin.parayre@gmail.com>
2020-10-26 10:53:15 +0100

    test email

--- src/components/contact/forms/index.js
+++ src/components/contact/forms/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 // Styling
 import "./index.css";
@@ -6,23 +6,64 @@ import "./index.css";
 // Components
 
 const Contact = () => {
+  const [handleName, setHandleName] = useState("");
+  const [handleEmail, setHandleEmail] = useState("");
+  const [handleFeedback, setHandleFeedback] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+  const handleNameChange = (event) => {
+    const value = event.target.value;
+    setHandleName(value);
+  };
+  const handleEmailChange = (event) => {
+    const value = event.target.value;
+    setHandleEmail(value);
+  };
+  const handleFeedbackChange = (event) => {
+    const value = event.target.value;
+    setHandleFeedback(value);
+  };
+
   return (
-    <forms className="forms-wrapper">
+    <form className="forms-wrapper" onSubmit={handleSubmit}>
       <div>
-        <input type="text" name="name" placeholder="Nom" required />
+        <input
+          type="text"
+          name="name"
+          placeholder="Nom"
+          value={handleName}
+          onChange={handleNameChange}
+          required
+        />
       </div>
       <div>
-        <input type="email" name="email" placeholder="Email" required />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          required
+          value={handleEmail}
+          onChange={handleEmailChange}
+        />
       </div>
       <div>
-        <textarea name="message" type="text" placeholder="message"></textarea>
+        <textarea
+          name="message"
+          type="text"
+          placeholder="message"
+          value={handleFeedback}
+          onChange={handleFeedbackChange}
+          required
+        ></textarea>
       </div>
       <div className="form-submit">
         <div>
-          <input type="button" value="Envoyer" />
+          <input type="button" value="Envoyer" onClick={handleSubmit} />
         </div>
       </div>
-    </forms>
+    </form>
   );
 };
 
